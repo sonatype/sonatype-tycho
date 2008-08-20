d58ee8d1460b49a58ff0dc6ed5a361e205dec48b
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-08-20 02:36:09 +0000

    integration test

---
+++ tycho-its/junit/p1.test/src/p1/test/ATest.java
@@ -0,0 +1,15 @@
+package p1.test;
+
+import junit.framework.TestCase;
+import p1.Activator;
+
+public class ATest extends TestCase {
+
+	public void testSuccess() {
+		assertEquals("p1.Activator", Activator.class.getName());
+	}
+	
+	public void _testFailure() {
+		assertTrue(false);
+	}
+}
