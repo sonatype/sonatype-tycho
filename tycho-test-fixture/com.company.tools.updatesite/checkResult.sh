125d1c4fd72f0b11ac07b837fa61d9ff344a166e
wes <wes@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-08-21 06:35:16 +0000

    fragment tests

--- tycho-test-fixture/com.company.tools.updatesite/checkResult.sh
+++ tycho-test-fixture/com.company.tools.updatesite/checkResult.sh
@@ -2,9 +2,16 @@
 # called to verify results of build
 
 scriptDir=`dirname "$0"`
-expectedFile="$scriptDir/target/site/site.xml"
-[ -f  "$expectedFile" ] || echo "## $0 FAIL: expected \"$expectedFile\" "
-expectedFile="$scriptDir/target/site/features/com.company.tools.core.feature_1.0.0.jar"
-## TODO enable when included-features are supported
-#[ -f  "$expectedFile" ] || echo "## $0 FAIL: expected \"$expectedFile\" "
+# site.xml:
+#   update site works
+# com.company.tools.core.feature_1.0.0.jar
+#   indirectly-reference feature
+# com.company.tools.packageService.user_1.0.0.jar
+#   if compiles ok, host fragment is properly visible to client of host
 
+#files="site.xml features/com.company.tools.core.feature_1.0.0.jar"
+files="site.xml features/com.company.tools.core.feature_1.0.0.jar plugins/com.company.packageService.provider.fragment"
+for e in $files; do
+	expectedFile="$scriptDir/target/site/$e"
+	[ -f  "$expectedFile" ] || echo "## $0 FAIL: expected \"$expectedFile\" "
+done
