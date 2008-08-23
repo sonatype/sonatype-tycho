1082249f1f3f46a28a0bcaf783d54a60f213dce4
wes <wes@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-08-23 20:47:17 +0000

    fragment not visible to client - need to investigate

--- tycho-test-fixture/com.company.packageService.provider/src/com/company/packageService/PackageProvider.java
+++ tycho-test-fixture/com.company.packageService.provider/src/com/company/packageService/PackageProvider.java
@@ -1,6 +1,13 @@
 package com.company.packageService;
 
 public class PackageProvider {
+	public static String getFragmentClassName() throws ClassNotFoundException {
+		return "com.company.packageService.FragmentProvider";
+	}
+	public static Class getFragmentClass() throws ClassNotFoundException {
+		ClassLoader loader = PackageProvider.class.getClassLoader();
+		return loader.loadClass(getFragmentClassName());
+	}
 	public static String providerName() {
 		return PackageProvider.class.getName();
 	}
--- tycho-test-fixture/com.company.packageService.user/src/com/company/packageService/user/Main.java
+++ tycho-test-fixture/com.company.packageService.user/src/com/company/packageService/user/Main.java
@@ -1,18 +1,25 @@
 package com.company.packageService.user;
 
 import com.company.packageService.PackageProvider;
-import com.company.packageService.FragmentProvider;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		String result = PackageProvider.providerName();
 		if (null == result) {
 			throw new Error("null PackageProvider result");
 		}
-		result = FragmentProvider.providerName();
-		// FragmentProvider exported by host
-		if (null == result) {
-			throw new Error("null FragmentProvider result");
+		Class c = getFragmentClass();
+		
+		if (null == c) {
+			throw new Error("null fragment class: " + getFragmentClassName());
 		}
+	}
+	// copied from provider
+	public static String getFragmentClassName() {
+		return "com.company.packageService.FragmentProvider";
+	}
+	public static Class getFragmentClass() throws ClassNotFoundException {
+		ClassLoader loader = PackageProvider.class.getClassLoader();
+		return loader.loadClass(getFragmentClassName());
 	}
 }
