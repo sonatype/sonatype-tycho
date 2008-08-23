0df5306724e2f967da7a2fbbda3eafc982d88159
wes <wes@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-08-23 20:51:47 +0000

    TYCHO-52 minimal acceptance test for second source directory

---
+++ tycho-test-fixture/com.company.tools.config/src/com/company/tools/config/FirstClass.java
@@ -0,0 +1,12 @@
+package com.company.tools.config;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		SecondClass.fromFirstClass(args);
+	}
+	
+	static void fromSecondClass(String[] args) {
+		
+	}
+}
---
+++ tycho-test-fixture/com.company.tools.config/src2/com/company/tools/config/SecondClass.java
@@ -0,0 +1,12 @@
+package com.company.tools.config;
+
+public class SecondClass {
+	public static void main(String[] args) {
+		FirstClass.fromSecondClass(args);
+	}
+
+	static void fromFirstClass(String[] args) {
+		
+	}
+	
+}
