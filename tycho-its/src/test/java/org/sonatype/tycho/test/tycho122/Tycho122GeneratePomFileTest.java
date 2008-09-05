ae723d707a056a4bd450e77492c1921aa7db40c5
velo <velo@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-09-05 20:43:24 +0000

    Ident code.

--- tycho-its/src/test/java/org/sonatype/tycho/test/tycho122/Tycho122GeneratePomFileTest.java
+++ tycho-its/src/test/java/org/sonatype/tycho/test/tycho122/Tycho122GeneratePomFileTest.java
@@ -8,16 +8,19 @@ import org.apache.maven.it.Verifier;
 import org.junit.Test;
 import org.sonatype.tycho.test.AbstractTychoIntegrationTest;
 
-public class Tycho122GeneratePomFileTest extends AbstractTychoIntegrationTest
+public class Tycho122GeneratePomFileTest
+    extends AbstractTychoIntegrationTest
 {
     @Test
-    public void generatePom() throws Exception {
-        Verifier verifier = getVerifier("/tycho122/tycho.demo");
+    public void generatePom()
+        throws Exception
+    {
+        Verifier verifier = getVerifier( "/tycho122/tycho.demo" );
 
         verifier.setAutoclean( false );
         verifier.executeGoal( "org.codehaus.tycho:maven-tycho-plugin:generate-poms" );
         verifier.verifyErrorFreeLog();
-        
+
         File pom = new File( verifier.getBasedir(), "pom.xml" );
         Assert.assertTrue( "Must generate the pom.xml", pom.exists() );
     }
