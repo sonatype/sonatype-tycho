761430c63c8ed1a2c65a3f0c17e6b90fab1dfacd
velo <velo@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-09-15 13:47:08 +0000

    OPEN - issue TYCHO-154: Test embed jars http://issues.sonatype.org/browse/TYCHO-154

---
+++ tycho-its/projects/tycho154/Bundle Jar Test/src/tests/BundleTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import simple.jar.SimpleClass;
+import junit.framework.TestCase;
+
+public class BundleTest extends TestCase {
+
+	public void testBundleJarAvaliability() {
+		new SimpleClass().sayHi();
+	}
+	
+}
