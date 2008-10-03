6acecfbb7ac74d4eceaf4dfa78d3dce246bec3c4
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-10-03 19:44:44 +0000

    TYCHO-177 target platform for tycho

---
+++ tp-scripts/tycho-e33.sh
@@ -0,0 +1,32 @@
+DIR=/home/j2ee-hudson
+TPDIR=$DIR/eclipse-tp-e33
+rm -rf $TPDIR
+mkdir -p $TPDIR
+
+# eclipse 3.3.2
+wget -P $TPDIR  -N http://archive.eclipse.org/eclipse/downloads/drops/R-3.3.2-200802211800/eclipse-SDK-3.3.2-linux-gtk-x86_64.tar.gz
+
+# RCP delta pack
+wget -P $TPDIR  -N http://archive.eclipse.org/eclipse/downloads/drops/R-3.3.2-200802211800/eclipse-RCP-3.3.2-delta-pack.zip
+
+
+
+#############################################################
+#############################################################
+#############################################################
+
+cd $TPDIR
+
+# install
+tar xfz *.tar.gz
+for i in *.zip
+do 
+  unzip -o $i 
+done
+
+#cd eclipse
+#for i in ../sites/*.zip
+#do 
+#  unzip -o $i 
+#done
+
