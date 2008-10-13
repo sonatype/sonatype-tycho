f385180fe0655989962c849c813a629d2459cae2
velo <velo@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-10-13 13:20:29 +0000

    OPEN - issue TYCHO-197: Create an integration test for qualifier http://issues.sonatype.org/browse/TYCHO-197

---
+++ tycho-its/src/test/java/org/sonatype/tycho/test/tycho197/Tycho197qualifierTest.java
@@ -0,0 +1,69 @@
+package org.sonatype.tycho.test.tycho197;
+
+import java.io.File;
+import java.util.jar.Manifest;
+
+import junit.framework.Assert;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.it.Verifier;
+import org.codehaus.tycho.model.Feature;
+import org.codehaus.tycho.model.PluginRef;
+import org.junit.Test;
+import org.sonatype.tycho.test.AbstractTychoIntegrationTest;
+
+import de.schlichtherle.io.FileInputStream;
+
+public class Tycho197qualifierTest extends AbstractTychoIntegrationTest {
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void test() throws Exception {
+		Verifier verifier = getVerifier("/tycho197");
+
+		final String timestamp = "20022002-2002";
+		verifier.getCliOptions().add("-DbuildNumber=" + timestamp);
+		verifier.executeGoal("install");
+		verifier.verifyErrorFreeLog();
+
+		File basedir = new File(verifier.getBasedir());
+		File site = new File(basedir, "Site/target/site");
+		Assert.assertTrue("Site folder should exists", site.isDirectory());
+
+		final String version = "1.0.0." + timestamp;
+		String featureLabel = "features/Feature_" + version + ".jar";
+
+		File siteXml = new File(site, "site.xml");
+		Assert.assertTrue("Site.xml should exists", siteXml.isFile());
+		String siteContet = FileUtils.readFileToString(siteXml);
+		Assert.assertTrue("Site.xml should contain '" + featureLabel
+				+ "'. Got:\n" + siteContet, siteContet.contains(featureLabel));
+
+		File feature = new File(site, featureLabel);
+		Assert.assertTrue("Feature '" + featureLabel + "' should exists",
+				feature.isFile());
+
+		de.schlichtherle.io.File featureJar = new de.schlichtherle.io.File(
+				feature, "feature.xml");
+		Feature featureXml = Feature.read(new FileInputStream(featureJar));
+		Assert.assertEquals("Invalid feature version", version, featureXml
+				.getVersion());
+
+		PluginRef pluginRef = featureXml.getPlugins().get(0);
+		Assert.assertEquals("Invalid plugin version at feature.xml", version,
+				pluginRef.getVersion());
+
+		String pluginLabel = "plugins/Plugin_" + version + ".jar";
+		File plugin = new File(site, pluginLabel);
+		Assert.assertTrue("Plugin '" + pluginLabel + "' should exists", plugin
+				.isFile());
+
+		de.schlichtherle.io.File manifest = new de.schlichtherle.io.File(
+				plugin, "META-INF/MANIFEST.MF");
+		Manifest man = new Manifest(new FileInputStream(manifest));
+		String bundleVersion = man.getMainAttributes().getValue(
+				"Bundle-Version");
+		Assert.assertEquals("Invalid Bundle-Version at plugin Manifest.MF",
+				version, bundleVersion);
+	}
+}
