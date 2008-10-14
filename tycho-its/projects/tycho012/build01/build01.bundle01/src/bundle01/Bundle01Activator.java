0517addbaef20714532c1046b320e5f73f3fa48c
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-10-14 21:06:18 +0000

    TYCHO-164 support for eclipse features

---
+++ tycho-its/projects/tycho012/build01/build01.bundle01/src/bundle01/Bundle01Activator.java
@@ -0,0 +1,18 @@
+package bundle01;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Bundle01Activator implements BundleActivator {
+
+	public void start(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
