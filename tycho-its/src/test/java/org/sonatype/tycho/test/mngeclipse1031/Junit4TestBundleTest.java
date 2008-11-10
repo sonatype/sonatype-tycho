64f96685b36eef318df95284fde9bcc983106649
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-11-10 06:28:37 +0000

    rolling back wrong change

--- tycho-its/src/test/java/org/sonatype/tycho/test/mngeclipse1031/Junit4TestBundleTest.java
+++ tycho-its/src/test/java/org/sonatype/tycho/test/mngeclipse1031/Junit4TestBundleTest.java
@@ -1,6 +1,11 @@
 package org.sonatype.tycho.test.mngeclipse1031;
 
+import static org.sonatype.tycho.test.util.EnvironmentUtil.isEclipse32Platform;
+
+import java.io.File;
+
 import org.apache.maven.it.Verifier;
+import org.junit.Assert;
 import org.junit.Test;
 import org.sonatype.tycho.test.AbstractTychoIntegrationTest;
 
@@ -9,12 +14,18 @@ public class Junit4TestBundleTest extends AbstractTychoIntegrationTest {
 	@Test
 	public void test() throws Exception {
 
-        Verifier verifier = getVerifier("MNGECLIPSE1026");
+		if(isEclipse32Platform()) {
+			// there is no junit4 support in eclipse 3.2
+			return;
+		}
 
-        verifier.executeGoal("package");
+        Verifier verifier = getVerifier("MNGECLIPSE1031/bundle.test");
+
+        verifier.executeGoal("integration-test");
         verifier.verifyErrorFreeLog();
-
-        // TODO actualy validate generated JAR file
+		
+        File testReport = new File(verifier.getBasedir(), "target/surefire-reports/TEST-bundle.test.BundleTest.xml");
+        Assert.assertTrue(testReport.exists());
 	}
 
 }
