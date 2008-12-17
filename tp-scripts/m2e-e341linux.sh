b54b94815831373ce421829a6a0a0380cb847381
eu <eu@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2008-12-17 00:37:24 +0000

    target platform for Eclipse 3.4.1

--- tp-scripts/m2e-e341linux.sh
+++ tp-scripts/m2e-e341linux.sh
@@ -12,24 +12,24 @@ wget -P $TPDIR  -N http://download.eclipse.org/eclipse/downloads/drops/R-3.4.1-2
 #wget -P $TPDIR  -N http://download.eclipse.org/eclipse/downloads/drops/R-3.4.1-200809111700/eclipse-SDK-3.4.1-win32-x86_64.zip
 #wget -P $TPDIR  -N http://download.eclipse.org/eclipse/downloads/drops/R-3.4.1-200809111700/eclipse-SDK-3.4.1-macosx-carbon.tar.gz
 
-# wtp&co 3.0 GA
-#wget -P $TPDIR  -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.0/R200806091234/emf-runtime-2.4.0.zip
-#wget -P $TPDIR  -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.0/R200806091234/xsd-runtime-2.4.0.zip
-#wget -P $TPDIR  -N http://download.eclipse.org/tools/gef/downloads/drops/3.4.0/R200806091334/GEF-SDK-3.4.0.zip
-#wget -P $TPDIR  -N http://download.eclipse.org/datatools/downloads/1.6/dtp-sdk_1.6.0.zip
-#wget -P $TPDIR  -N http://download.eclipse.org/webtools/downloads/drops/R3.0/R-3.0-20080616152118/wtp-sdk-R-3.0-20080616152118a.zip
-#wget -P $TPDIR   -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.1/R200808251517/emf-runtime-2.4.1.zip
-#wget -P $TPDIR   -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.1/R200808251517/xsd-runtime-2.4.1.zip
-#wget -P $TPDIR   -N http://download.eclipse.org/tools/gef/downloads/drops/3.4.1/R200809101400/GEF-SDK-3.4.1.zip
-wget -P $TPDIR   -N http://download.eclipse.org/datatools/downloads/1.6/dtp-sdk_1.6.1.zip
-wget -P $TPDIR   -N http://download.eclipse.org/webtools/downloads/drops/R3.0/R-3.0.3-20081113203138/wtp-sdk-R-3.0.3-20081113203138.zip
+# WTP 3.0.3
+#wget -P $TPDIR/features  -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.0/R200806091234/emf-runtime-2.4.0.zip
+#wget -P $TPDIR/features  -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.0/R200806091234/xsd-runtime-2.4.0.zip
+#wget -P $TPDIR/features  -N http://download.eclipse.org/tools/gef/downloads/drops/3.4.0/R200806091334/GEF-SDK-3.4.0.zip
+#wget -P $TPDIR/features  -N http://download.eclipse.org/datatools/downloads/1.6/dtp-sdk_1.6.0.zip
+#wget -P $TPDIR/features  -N http://download.eclipse.org/webtools/downloads/drops/R3.0/R-3.0-20080616152118/wtp-sdk-R-3.0-20080616152118a.zip
+#wget -P $TPDIR/features   -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.1/R200808251517/emf-runtime-2.4.1.zip
+#wget -P $TPDIR/features   -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.1/R200808251517/xsd-runtime-2.4.1.zip
+#wget -P $TPDIR/features   -N http://download.eclipse.org/tools/gef/downloads/drops/3.4.1/R200809101400/GEF-SDK-3.4.1.zip
+wget -P $TPDIR/features   -N http://download.eclipse.org/datatools/downloads/1.6/dtp-sdk_1.6.1.zip
+wget -P $TPDIR/features   -N http://download.eclipse.org/webtools/downloads/drops/R3.0/R-3.0.3-20081113203138/wtp-sdk-R-3.0.3-20081113203138.zip
 
 # emf 2.4.1
 wget -P $TPDIR/features   -N http://download.eclipse.org/modeling/emf/emf/downloads/drops/2.4.1/R200808251517/emf-sdo-xsd-SDK-2.4.1.zip
 
 # zest 3.4 GA
-#wget -P $TPDIR  -N http://download.eclipse.org/tools/gef/downloads/drops/3.4.0/R200806091334/GEF-zest-sdk-3.4.0.zip
-wget -P $TPDIR  -N http://download.eclipse.org/tools/gef/downloads/drops/3.4.1/R200809101400/GEF-ALL-3.4.1.zip
+#wget -P $TPDIR/features  -N http://download.eclipse.org/tools/gef/downloads/drops/3.4.0/R200806091334/GEF-zest-sdk-3.4.0.zip
+wget -P $TPDIR/features  -N http://download.eclipse.org/tools/gef/downloads/drops/3.4.1/R200809101400/GEF-ALL-3.4.1.zip
 
 # AJDT 1.6.0
 wget -P $TPDIR/sites  -N http://download.eclipse.org/tools/ajdt/34/update/ajdt_1.6.0_for_eclipse_3.4.zip
@@ -60,7 +60,7 @@ tar xfz $FILES/eclipse-SDK-3.4.1-linux-gtk.tar.gz
 tar xfz $FILES/eclipse-SDK-3.4.1-linux-gtk-x86_64.tar.gz
 tar xfz $FILES/eclipse-SDK-3.4.1-macosx-carbon.tar.gz   
 
-# install everything under dropts 
+# install everything under dropts
 # eclipse does not immediately scan plugins folder for new bundles
 
 for i in $FILES/*.zip
