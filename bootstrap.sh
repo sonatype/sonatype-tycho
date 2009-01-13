584df4098ca533b0113182187f4c3be901d4c7e7
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-01-13 04:39:36 +0000

    build script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,33 @@
+#!/bin/bash
+
 BASDIR=$(pwd)
+
+
+# Location of Eclipse 3.4.1 with RCP Delta pack
+# TYCHO_TARGET_PLATFORM=/var/tmp/e34-rcp/eclipse/
+
+
+# location of maven 3.0 used to build bootstrap tycho distribution
+# M2_HOME=/opt/maven
+
+# Tycho bootstrap distribution location
 TYCHO_BOOTSTRAP=$BASDIR/tycho-bootstrap
-TARGET_PLATFORM=/opt/eclipse-3.4/eclipse
-M2_HOME=/opt/maven
 
 
-$M2_HOME/bin/mvn clean install -B -U -e
 
-# Build for real using bootstrap tycho from above
+# Stage 1, build bootstrap tycho distribution
+$M2_HOME/bin/mvn clean install -e
 rm -rf $TYCHO_BOOTSTRAP; mkdir -p `dirname $TYCHO_BOOTSTRAP`
 cp -R $BASDIR/tycho-distribution/target/tycho-distribution-*-bin.dir/tycho-distribution-* $TYCHO_BOOTSTRAP
 chmod +x $TYCHO_BOOTSTRAP/bin/mvn
-env M2_HOME=$TYCHO_BOOTSTRAP \
-  $TYCHO_BOOTSTRAP/bin/mvn -Pdeploy clean install -B -U -e \
-  -Dtycho.targetPlatform=$TARGET_PLATFORM
 
-rm -rf $TYCHO_BOOTSTRAP
+
+
+# Stage 2, build full tycho distribution and run integration tests
+env M2_HOME=$TYCHO_BOOTSTRAP \
+  $TYCHO_BOOTSTRAP/bin/mvn -Pits clean install -e \
+  -Dtycho.targetPlatform=$TYCHO_TARGET_PLATFORM
+
+
+# cleanup
+#rm -rf $TYCHO_BOOTSTRAP
