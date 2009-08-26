f9e3e6459b9e2fc21418d4920375a9f82c86937c
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-08-26 17:52:56 +0000

    TYCHO-274 support remote tycho repositories in p2 target platform resolver

---
+++ tycho-its/src/test/java/org/sonatype/tycho/test/TYCHO0164tycho03RepositoryRoundtrip/Tycho03RepositoryRoundtripTest.java
@@ -0,0 +1,29 @@
+package org.sonatype.tycho.test.TYCHO0164tycho03RepositoryRoundtrip;
+
+import java.io.File;
+
+import org.apache.maven.it.Verifier;
+import org.junit.Assert;
+import org.junit.Test;
+import org.sonatype.tycho.test.AbstractTychoIntegrationTest;
+
+public class Tycho03RepositoryRoundtripTest extends AbstractTychoIntegrationTest {
+
+	@Test
+	public void testLocalMavenRepository() throws Exception {
+        Verifier v01 = getVerifier("TYCHO0164tycho03RepositoryRoundtrip/build01", false);
+    	v01.getCliOptions().add( "-DtargetPlatform=" + getTargetPlatforn() );
+        v01.executeGoal("install");
+        v01.verifyErrorFreeLog();
+
+        Verifier v02 = getVerifier("TYCHO0164tycho03RepositoryRoundtrip/build02", false);
+    	v02.getCliOptions().add( "-DtargetPlatform=" + getTargetPlatforn() );
+        v02.executeGoal("install");
+        v02.verifyErrorFreeLog();
+
+        File site = new File(v02.getBasedir(), "build02.site01/target/site");
+
+        Assert.assertEquals(2, new File(site, "features").listFiles().length);
+        Assert.assertEquals(3, new File(site, "plugins").listFiles().length);
+	}
+}
---
+++ tycho-its/src/test/java/org/sonatype/tycho/test/TYCHO0209tychoRepositoryRoundtrip/TychoRepositoryRoundtripTest.java
@@ -0,0 +1,80 @@
+package org.sonatype.tycho.test.TYCHO0209tychoRepositoryRoundtrip;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.maven.it.Verifier;
+import org.codehaus.plexus.util.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.sonatype.tycho.p2.facade.internal.DefaultTychoRepositoryIndex;
+import org.sonatype.tycho.test.AbstractTychoIntegrationTest;
+
+public class TychoRepositoryRoundtripTest
+    extends AbstractTychoIntegrationTest
+{
+
+    @Test
+    public void testLocalMavenRepository()
+        throws Exception
+    {
+        Verifier v01 = getVerifier( "TYCHO0209tychoRepositoryRoundtrip/build01", false );
+        v01.getCliOptions().add( "-Dp2.repo=" + toURI( new File( "repositories/e342" ) ) );
+        v01.getCliOptions().add( "-Dmaven.test.skip=true" );
+        v01.executeGoal( "install" );
+        v01.verifyErrorFreeLog();
+
+        Verifier v02 = getVerifier( "TYCHO0209tychoRepositoryRoundtrip/build02", false );
+        v02.getCliOptions().add( "-Dp2.repo=" + toURI( new File( "repositories/e342" ) ) );
+        v02.executeGoal( "install" );
+        v02.verifyErrorFreeLog();
+
+        File site = new File( v02.getBasedir(), "build02.site01/target/site" );
+
+        Assert.assertEquals( 2, new File( site, "features" ).listFiles().length );
+        Assert.assertEquals( 3, new File( site, "plugins" ).listFiles().length );
+    }
+
+    @Test
+    public void testRemoteRepository()
+        throws Exception
+    {
+        // cleanup old tycho index
+        File localBasedir = new File( getLocalRepo() );
+        new File( localBasedir, DefaultTychoRepositoryIndex.INDEX_RELPATH ).delete();
+
+        // install build01
+        Verifier v01 = getVerifier( "TYCHO0209tychoRepositoryRoundtrip/build01", false );
+        v01.getCliOptions().add( "-Dp2.repo=" + toURI( new File( "repositories/e342" ) ) );
+        v01.getCliOptions().add( "-Dmaven.test.skip=true" );
+        v01.executeGoal( "install" );
+        v01.verifyErrorFreeLog();
+
+        // now lets fake remote repo
+        String build01relpath = "org/codehaus/tycho/tychoits/tycho0209/build01";
+        File remoteBasedir = new File( "target/remoterepo/" );
+        FileUtils.copyDirectory( new File( localBasedir, build01relpath ), new File( remoteBasedir, build01relpath ) );
+        FileUtils.copyFile( new File( localBasedir, DefaultTychoRepositoryIndex.INDEX_RELPATH), new File( remoteBasedir, DefaultTychoRepositoryIndex.INDEX_RELPATH ) );
+
+        // cleanup localrepo once again
+        new File( localBasedir, DefaultTychoRepositoryIndex.INDEX_RELPATH ).delete();
+        FileUtils.deleteDirectory( new File( remoteBasedir, build01relpath ) );
+
+        // build02
+        Verifier v02 = getVerifier( "TYCHO0209tychoRepositoryRoundtrip/build02", false );
+        v02.getCliOptions().add( "-Dp2.repo=" + toURI( new File( "repositories/e342" ) ) );
+        v02.getCliOptions().add( "-Dremote.repo=" +  toURI( remoteBasedir ) );
+        v02.executeGoal( "install" );
+        v02.verifyErrorFreeLog();
+
+        File site = new File( v02.getBasedir(), "build02.site01/target/site" );
+
+        Assert.assertEquals( 2, new File( site, "features" ).listFiles().length );
+        Assert.assertEquals( 3, new File( site, "plugins" ).listFiles().length );
+    }
+
+    private String toURI( File file ) throws IOException
+    {
+        return file.getCanonicalFile().toURI().normalize().toString();
+    }
+}
