79de59cce492fe87520c355e68e8d8fcaafb2dde
ifedorenko <ifedorenko@42f58313-09e3-4aa2-bb18-3b3e6a6bc9e8>
2009-10-29 20:24:47 +0000

    investigating source bundle resolution problems.

---
+++ tycho-p2/org.sonatype.tycho.p2.impl.test/src/test/java/com/sonatype/nexus/p2/impl/test/ConsoleProgressMonitor.java
@@ -0,0 +1,45 @@
+package com.sonatype.nexus.p2.impl.test;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class ConsoleProgressMonitor
+    implements IProgressMonitor
+{
+
+    public void beginTask( String name, int totalWork )
+    {
+        System.out.println( name );
+    }
+
+    public void done()
+    {
+    }
+
+    public void internalWorked( double work )
+    {
+    }
+
+    public boolean isCanceled()
+    {
+        return false;
+    }
+
+    public void setCanceled( boolean value )
+    {
+    }
+
+    public void setTaskName( String name )
+    {
+        System.out.println( name );
+    }
+
+    public void subTask( String name )
+    {
+        System.out.println( name );
+    }
+
+    public void worked( int work )
+    {
+    }
+
+}
--- tycho-p2/org.sonatype.tycho.p2.impl.test/src/test/java/com/sonatype/nexus/p2/impl/test/GalileoProxyTest.java
+++ tycho-p2/org.sonatype.tycho.p2.impl.test/src/test/java/com/sonatype/nexus/p2/impl/test/GalileoProxyTest.java
@@ -1,36 +1,69 @@
 package com.sonatype.nexus.p2.impl.test;
 
-import java.io.OutputStream;
+import java.io.File;
 import java.net.URI;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.equinox.internal.p2.artifact.repository.simple.SimpleArtifactRepository;
 import org.eclipse.equinox.internal.p2.artifact.repository.simple.SimpleArtifactRepositoryIO;
 import org.eclipse.equinox.internal.p2.core.helpers.ServiceHelper;
 import org.eclipse.equinox.internal.provisional.p2.artifact.repository.IArtifactRepository;
 import org.eclipse.equinox.internal.provisional.p2.artifact.repository.IArtifactRepositoryManager;
+import org.eclipse.equinox.internal.provisional.p2.metadata.IInstallableUnit;
+import org.eclipse.equinox.internal.provisional.p2.metadata.query.InstallableUnitQuery;
+import org.eclipse.equinox.internal.provisional.p2.metadata.repository.IMetadataRepository;
+import org.eclipse.equinox.internal.provisional.p2.metadata.repository.IMetadataRepositoryManager;
+import org.eclipse.equinox.internal.provisional.p2.query.Collector;
 import org.junit.Test;
 import org.sonatype.tycho.p2.Activator;
 
 @SuppressWarnings( "restriction" )
 public class GalileoProxyTest
 {
-    private IProgressMonitor monitor = new NullProgressMonitor();
-    
-    @Test
-    public void test() throws Exception
-    {
-        IArtifactRepositoryManager artifactRepositoryManager = (IArtifactRepositoryManager) ServiceHelper.getService(
-            Activator.getContext(),
-            IArtifactRepositoryManager.class.getName() );
+    private IProgressMonitor monitor = new ConsoleProgressMonitor();
 
-        URI location = new URI( "http://download.eclipse.org/releases/galileo" );
+    private URI location;
+
+    public GalileoProxyTest()
+        throws Exception
+    {
+        location = new URI( "http://download.eclipse.org/releases/galileo" );
+    }
+
+    @Test
+    public void testArtifactRepository()
+        throws Exception
+    {
+        IArtifactRepositoryManager artifactRepositoryManager =
+            (IArtifactRepositoryManager) ServiceHelper.getService( Activator.getContext(),
+                                                                   IArtifactRepositoryManager.class.getName() );
+
         IArtifactRepository artifactRepository = artifactRepositoryManager.loadRepository( location, monitor );
         SimpleArtifactRepository simple =
             (SimpleArtifactRepository) artifactRepository.getAdapter( SimpleArtifactRepository.class );
         new SimpleArtifactRepositoryIO().write( simple, System.out );
 
         System.out.println( simple );
+    }
+
+    @Test
+    public void testMetadataRepository()
+        throws Exception
+    {
+        IMetadataRepositoryManager repositoryManager =
+            (IMetadataRepositoryManager) ServiceHelper.getService( Activator.getContext(),
+                                                                   IMetadataRepositoryManager.class.getName() );
+
+        IMetadataRepository repository = repositoryManager.loadRepository( location, monitor );
+
+        File localFile = new File( "/tmp/xxx" );
+        IMetadataRepository localRepository =
+            repositoryManager.createRepository( localFile.toURI(), localFile.getName(),
+                                                IMetadataRepositoryManager.TYPE_SIMPLE_REPOSITORY, null );
+        repositoryManager.removeRepository( localFile.toURI() );
+
+        Collector collector = repository.query( InstallableUnitQuery.ANY, new Collector(), monitor );
+
+        localRepository.addInstallableUnits( (IInstallableUnit[]) collector.toArray( IInstallableUnit.class ) );
     }
 }
