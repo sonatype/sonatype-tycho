50e784199e9c9fdf0a4d6a2226bc5cab9775b669
Igor Fedorenko <igor@ifedorenko.com>
2010-02-01 22:13:23 -0500

    TYCHO-340 regression in integration test
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- tycho-its/src/test/java/org/sonatype/tycho/test/TYCHO340explodedTestDependencies/ExplodedTestDependenciesTest.java
+++ tycho-its/src/test/java/org/sonatype/tycho/test/TYCHO340explodedTestDependencies/ExplodedTestDependenciesTest.java
@@ -7,18 +7,21 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.sonatype.tycho.test.AbstractTychoIntegrationTest;
 
-public class ExplodedTestDependenciesTest extends AbstractTychoIntegrationTest {
+public class ExplodedTestDependenciesTest
+    extends AbstractTychoIntegrationTest
+{
 
-	@Test
-	public void testLocalMavenRepository() throws Exception {
-		Verifier v01 = getVerifier("TYCHO340explodedTestDependencies", false);
-		v01.executeGoal("install");
-		v01.verifyErrorFreeLog();
+    @Test
+    public void testLocalMavenRepository()
+        throws Exception
+    {
+        Verifier v01 = getVerifier( "TYCHO340explodedTestDependencies", false );
+        v01.executeGoal( "install" );
+        v01.verifyErrorFreeLog();
 
-		File antHome = new File(
-				v01.localRepo,
-				"p2/osgi/bundle/org.apache.ant/1.7.1.v20090120-1145/org.apache.ant_1.7.1.v20090120-1145");
-		Assert.assertTrue(antHome.isDirectory());
-	}
+        File antHome =
+            new File( v01.getBasedir(), "tycho340.test/target/work/plugins/org.apache.ant_1.7.1.v20090120-1145" );
+        Assert.assertTrue( antHome.isDirectory() );
+    }
 
 }
