0738799d18ac01e95eacacf0070b7cf5c57c34dd
Igor Fedorenko <igor@ifedorenko.com>
2010-06-08 12:19:25 -0400

    actually pass it.cliOptions to the forked test jvm
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,8 +10,10 @@ TYCHO_M2_HOME=/opt/maven
 
 export MAVEN_OPTS=-Xmx512m
 
+export MAVEN_PARAMS="-Dmaven.repo.local=/tmp/localrepo -Dit.cliOptions=-U"
+
 $TYCHO_M2_HOME/bin/mvn clean install -e -V -Pbootstrap-1 || exit
 
 $TYCHO_M2_HOME/bin/mvn clean install -e -V -Pbootstrap-2 -Dtycho.targetPlatform=$TYCHO_TARGET_PLATFORM || exit
 
-$TYCHO_M2_HOME/bin/mvn clean install -e -V -Pits -Dtycho.targetPlatform=$TYCHO_TARGET_PLATFORM || exit
+$TYCHO_M2_HOME/bin/mvn clean install -U -up -e -V -Pits -Dtycho.targetPlatform=$TYCHO_TARGET_PLATFORM ${MAVEN_PARAMS} || exit
