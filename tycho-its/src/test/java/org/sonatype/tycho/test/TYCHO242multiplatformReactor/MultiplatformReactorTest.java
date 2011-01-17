4a516c9c80f477cc5751a0eac1e607a329b87532
Tobias Oberlies <tobias.oberlies@sap.com>
2011-01-17 17:35:54 +0100

    TYCHO-526 Run multiplatform integration test with p2 resolver
    With this commit, the integration test TYCHO242multiplatformReactor
    is executed with the p2 target platform resolver instead of the local
    resolver. For this to work, the target environments configuration in
    the parent POM had to be corrected.
    
    Includes the following, additional changes:
    - Extende test project to include an OS X fragment and a deployable
      feature (based on Igor's patch attached to TYCHO-526.)
    - Define target environments with non-deprecated syntax.
    - Use ws/os/arch for which launchers are available in checked-in
      repository (to run locally, set -DtestRepository=
      file:/<git root>/sonatype-tycho/tycho-its/repositories/e342/ )
    - Define environment correctly for assembly. E.g. the feature needs
      all fragments in its target platform so that it can replace the
      0.0.0 by correct versions.

--- tycho-its/src/test/java/org/sonatype/tycho/test/TYCHO242multiplatformReactor/MultiplatformReactorTest.java
+++ tycho-its/src/test/java/org/sonatype/tycho/test/TYCHO242multiplatformReactor/MultiplatformReactorTest.java
@@ -14,16 +14,16 @@ public class MultiplatformReactorTest
     public void testMultiplatformReactorBuild()
         throws Exception
     {
-        Verifier verifier = getVerifier( "/TYCHO242multiplatformReactor" );
+        Verifier verifier = getVerifier( "/TYCHO242multiplatformReactor", false );
+        verifier.getCliOptions().add( "-DtestRepository=" + new File( "repositories/e342" ).getCanonicalFile().toURI() );
         verifier.executeGoal( "integration-test" );
         verifier.verifyErrorFreeLog();
 
-
         // assert product got proper platform fragments 
         File productTarget = new File( verifier.getBasedir(), "product/target" );
-        assertFileExists( productTarget, "linux.gtk.x86/eclipse/plugins/fragment.linux_0.0.1.*.jar" );
+        assertFileExists( productTarget, "linux.gtk.x86_64/eclipse/plugins/fragment.linux_0.0.1.*.jar" );
         assertFileExists( productTarget, "win32.win32.x86/eclipse/plugins/fragment.windows_0.0.1.*.jar" );
-        
+
         // assert site got all platform fragments
         File siteproductTarget = new File( verifier.getBasedir(), "site/target" );
         assertFileExists( siteproductTarget, "site/plugins/fragment.linux_0.0.1.*.jar" );
