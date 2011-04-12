15ca9327ef52c1f6a9b7e90aa98f709242b12be7
Jan Sievers <jan.sievers@sap.com>
2011-04-12 15:43:47 +0200

    Remove accidentally added EPL headers
    EPL headers were added accidentally because
    existing apache license was not at start of file.

--- tycho-surefire/org.eclipse.tycho.surefire.junit/src/org/eclipse/tycho/surefire/junit/JUnitTestSet.java
+++ tycho-surefire/org.eclipse.tycho.surefire.junit/src/org/eclipse/tycho/surefire/junit/JUnitTestSet.java
@@ -1,15 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2008, 2011 Sonatype Inc. and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *    Sonatype Inc. - initial API and implementation
- *******************************************************************************/
-package org.eclipse.tycho.surefire.junit;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -28,6 +16,7 @@ package org.eclipse.tycho.surefire.junit;
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.eclipse.tycho.surefire.junit;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
